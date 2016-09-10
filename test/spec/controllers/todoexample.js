883418ae609a6af1310d80e5fd2f1df53f9e3bf4
Ernesto Perez <ernespn@gmail.com>
2016-09-10 20:35:59 +0100

    removing test cases that are not relevant anymore

--- test/spec/controllers/todoexample.js
+++ test/spec/controllers/todoexample.js
@@ -21,13 +21,13 @@ describe('Controller: TodoexampleCtrl', function () {
   it('should have no items to start', function () {
     expect(scope.todos.length).toBe(0);
   });
-    
+
     it('should add items to the list', function(){
         scope.todo  = 'Test 1';
         scope.addTodo();
         expect(scope.todos.length).toBe(1);
     });
-    
+
     it('should add then remove item from the list', function(){
         scope.todo = 'Test 1';
         scope.addTodo();
