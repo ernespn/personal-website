60bdc4180baaed211e34bf1116e30d415a4e6458
Ernesto Perez <ernespn@gmail.com>
2016-09-26 23:25:05 +0100

    connect properly microservices.

--- app/scripts/controllers/microservices.js
+++ app/scripts/controllers/microservices.js
@@ -8,13 +8,22 @@
  * Controller of the websiteApp
  */
 angular.module('websiteApp')
-  .controller('MicroservicesCtrl', function ($scope) {
+  .controller('MicroservicesCtrl', function ($scope, $http) {
     $scope.result = '';
     $scope.from = '';
     $scope.submit = function (){
+       
         if ($scope.num1 && $scope.num2){
-          $scope.result = parseInt($scope.num1) + parseInt($scope.num2);
-          $scope.from = 'Angular generation';
+          var url = 'http://localhost:8080/add/'+$scope.num1+'/'+$scope.num2;
+          $http.get(url).success( function(response) {
+            $scope.result = response.result; 
+            $scope.from = response.from; 
+          });
+          
+          // $scope.result = parseInt($scope.num1) + parseInt($scope.num2);
+          // $scope.from = 'Angular generation';
         }
     };
+
+
 });
