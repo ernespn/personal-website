5da061bacb237dcca57bc5c742d9b8ee2b3827a7
Ernesto Pérez Nieto <ernesto.pereznieto@abcam.com>
2016-10-18 12:57:54 +0100

    Change host

--- app/scripts/controllers/microservices.js
+++ app/scripts/controllers/microservices.js
@@ -8,20 +8,17 @@
  * Controller of the websiteApp
  */
 angular.module('websiteApp')
-  .controller('MicroservicesCtrl', function ($scope, $http) {
+  .controller('MicroservicesCtrl', function ($scope, $http, $location) {
     $scope.result = '';
     $scope.from = '';
     $scope.submit = function (){
        
         if ($scope.num1 && $scope.num2){
-          var url = 'http://localhost:8080/add/'+$scope.num1+'/'+$scope.num2;
+          var url = 'http://'+$location.host()+':8080/add/'+$scope.num1+'/'+$scope.num2;
           $http.get(url).success( function(response) {
             $scope.result = response.result; 
             $scope.from = response.from; 
           });
-          
-          // $scope.result = parseInt($scope.num1) + parseInt($scope.num2);
-          // $scope.from = 'Angular generation';
         }
     };
 
