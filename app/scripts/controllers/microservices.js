e7e47ee06754c3bafaa96cb940b99588b81e7a57
Ernesto Pérez Nieto <ernesto.pereznieto@abcam.com>
2016-10-26 16:19:22 +0100

    Add GA and some styling and text

--- app/scripts/controllers/microservices.js
+++ app/scripts/controllers/microservices.js
@@ -14,13 +14,19 @@ angular.module('websiteApp')
     $scope.submit = function (){
        
         if ($scope.num1 && $scope.num2){
-          var url = 'http://'+$location.host()+':8080/add/'+$scope.num1+'/'+$scope.num2;
+          var url = 'http://'+$scope.subdomain()+':8080/add/'+$scope.num1+'/'+$scope.num2;
           $http.get(url).success( function(response) {
             $scope.result = response.result; 
             $scope.from = response.from; 
           });
         }
     };
-
-
+    
+    $scope.subdomain = function(){
+      var host = $location.host();
+      if (host.indexOf('.') < 0) 
+         return null;
+      else
+        return host.split('.')[0];
+    };
 });
