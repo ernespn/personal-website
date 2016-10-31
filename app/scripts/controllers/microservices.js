160c69e4db99d60408cd7cd1fe8136651ee5b3bd
Ernesto Perez <ernespn@gmail.com>
2016-10-31 16:46:17 +0000

    add todo example

--- app/scripts/controllers/microservices.js
+++ app/scripts/controllers/microservices.js
@@ -14,19 +14,28 @@ angular.module('websiteApp')
     $scope.submit = function (){
        
         if ($scope.num1 && $scope.num2){
-          var url = 'http://'+$location.host()+':8080/add/'+$scope.num1+'/'+$scope.num2;
+          var url = 'http://'+$scope.getUrl()+'/add/'+$scope.num1+'/'+$scope.num2;
           $http.get(url).success( function(response) {
             $scope.result = response.result; 
             $scope.from = response.from; 
           });
         }
     };
-    
+
+    $scope.getUrl = function (){
+      var host = $location.host();
+      var subdomain = $scope.subdomain();
+      if (subdomain === 'srv'){
+         return host;
+      }
+      return host+':8080';
+    };
+
     $scope.subdomain = function(){
       var host = $location.host();
-      if (host.indexOf('.') < 0) 
+      if (host.indexOf('.') < 0) {
          return null;
-      else
-        return host.split('.')[0];
+      }
+      return host.split('.')[0];
     };
 });
--- app/scripts/controllers/todoexample.js
+++ app/scripts/controllers/todoexample.js
@@ -8,14 +8,28 @@
  * Controller of the websiteApp
  */
 angular.module('websiteApp')
-  .controller('TodoexampleCtrl', function ($scope) {
+  .controller('TodoexampleCtrl', function ($scope, $http, $location) {
+    $scope.url = 'http://' + $location.host() + ':8070/todo';
     $scope.todos = [];
+    $scope.getAllTodos = function (){
+      $http.get($scope.url)
+      .success(function(data) {
+        $scope.todos = data;
+      })
+      .error(function (data, status, header, config) {
+        console.log('Data:'+data+'hr />status: ' + status + '<hr />headers: ' + header + '<hr />config: ' + config );
+      });
+    };
     $scope.addTodo = function(){
-        $scope.todos.push($scope.todo);
+        var config = { headers: { 'Content-Type': 'application/json'} };
+        var data = { what : $scope.todo };
+        $http.post($scope.url, data, config).then(function() { $scope.getAllTodos(); });
         $scope.todo = '';
     };
-    $scope.removeTodo = function(index){
-        $scope.todos.splice(index, 1);
+    $scope.removeTodo = function(id){
+        var config = { headers: { 'Content-Type': 'application/json'}};
+        $http.delete($scope.url+'/'+id, config).then(function() { $scope.getAllTodos(); });
     };
+    $scope.loadData = $scope.getAllTodos();
 
   });
